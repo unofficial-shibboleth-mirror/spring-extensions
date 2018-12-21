46a95b136fc3a35f76da46bbeb9cb9de2338a8d6
Rod Widdowson <rdw@steadingsoftware.com>
2018-12-21 09:20:57 +0000

    JSE-30 Supress HTTPResourceTest#springLoadFileCache
    https://issues.shibboleth.net/jira/browse/JSE-30
    
    It leaves a mess on the tmp dir, and as currently configured doesn't
    test what it is supposed to

--- src/test/java/net/shibboleth/ext/spring/resource/HTTPResourceTest.java
+++ src/test/java/net/shibboleth/ext/spring/resource/HTTPResourceTest.java
@@ -200,7 +200,7 @@ public class HTTPResourceTest {
         Assert.assertTrue(dir.delete());
     }
 
-    @Test public void springLoadFileCache() throws IOException {
+    @Test(enabled=false) public void springLoadFileCache() throws IOException {
         File theDir = null;
         GenericApplicationContext context = null;
         try {
